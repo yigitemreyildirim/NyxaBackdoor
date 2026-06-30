1cde25d89d5d5c66a17d4ce35618673935e83178
Yiğit Emre Yıldırım <yldrmyigit00@gmail.com>
2026-06-30 01:18:59 -0700

    Add socket listener for incoming connections

--- nyxa_socket_listener.py
+++ nyxa_socket_listener.py
@@ -1,1 +1,9 @@
+import socket
 
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+listener.bind(("192.168.1.59",8080))
+listener.listen(0)
+print("listening on")
+listener.accept()
+print("Connected")
